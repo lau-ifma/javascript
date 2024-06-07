fa87c37a01b9099dcd42545072705cf272e8fa32
Lauanderson <161143157+lau-ifma@users.noreply.github.com>
2024-06-07 01:19:12 -0300

    Update tailwind.config.js

--- PORTIFOLIO-JS/tailwind.config.js
+++ PORTIFOLIO-JS/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
         neue: [ "Comic Neue", 'cursive'],
         machina: ["Neue Machina", 'sans-serif']
+      },
+      backgroundImage: {
+        'degrade-roxo': "url('./assets/gradient-roxo-canto.png')",
       },
     },
   },
